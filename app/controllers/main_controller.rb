3602e2e6b0eb184e61bc2bfb1aa88f0ff4cf99c0
kartikay Kala <kartikay304@gmail.com>
2021-09-10 19:09:26 +0530

    Added flash messages

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,6 @@
 class MainController < ApplicationController
     def index
+        flash.now[:notice] = "Logged in successfully"
+        flash.now[:alert] = "Invalid email or password"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get "about", to: "about#index"
+  get "about-us", to: "about#index", as: :about
 
   root to: "main#index"
 end
